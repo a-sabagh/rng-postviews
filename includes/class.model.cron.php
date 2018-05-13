dde29896149c350b441e112a875a885769d686c7
asabagh <a.sabagh72@gmail.com>
2018-05-14 02:20:36 +0430

    update function update_db_cron_day

--- includes/class.model.cron.php
+++ includes/class.model.cron.php
@@ -9,12 +9,12 @@ if (!class_exists("ja_cron_model")) {
             $wpdb->query( "START TRANSACTION" );
             $result = array();
             $result[] = update_option("ja_postviews_day_first", 0);
-            $result[] = update_option("ja_postviews_day_second", 0);
-            $result[] = update_option("ja_postviews_day_third", 0);
-            $result[] = update_option("ja_postviews_day_fourth", 0);
-            $result[] = update_option("ja_postviews_day_fifth", 0);
-            $result[] = update_option("ja_postviews_day_sixth", 0);
-            $result[] = update_option("ja_postviews_day_seventh", 0);
+            $result[] = update_option("ja_postviews_day_second", current($args));
+            $result[] = update_option("ja_postviews_day_third", next($args));
+            $result[] = update_option("ja_postviews_day_fourth", next($args));
+            $result[] = update_option("ja_postviews_day_fifth", next($args));
+            $result[] = update_option("ja_postviews_day_sixth", next($args));
+            $result[] = update_option("ja_postviews_day_seventh", next($args));
             if(in_array(FALSE, $result)){
                 $wpdb->query( "ROLLBACK" );
             } else {
