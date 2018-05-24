9eea71b4a3c8bbc7211ee5ef836f1786ba837daa
asabagh <a.sabagh72@gmail.com>
2018-05-24 18:08:19 +0430

    ading new style for table

--- admin/mail-body.php
+++ admin/mail-body.php
@@ -1,51 +1,54 @@
-<?php 
-$views = __(" views","rng-postviews");
+<?php
+$views = __(" views", "rng-postviews");
 ?>
-<div style="display: block;overflow: auto;">
-    <table style="width: 80%;text-align: center;border-collapse: collapse;margin: auto; font-family: arial;">
-        <thead>
-            <tr style="border-bottom: 1px solid #ddd;">
-                <th><?php _e("Date","rng-postviews"); ?></th>
-                <th><?php _e("Post Views","rng-postviews"); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo current($days_period) . __("(Today)","rng-postviews") ?></td>
-                <td style="padding: 6px 5px;"><?php echo current($days_postviews) . $views; ?></td>
-            </tr>
-            <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
-                <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
-            </tr>
-            <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php _e("Current Week","rng-postviews"); ?></td>
-                <td style="padding: 6px 5px;"><?php echo current($weeks_postviews) . $views; ?></td>
-            </tr>
-            <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                <td style="padding: 6px 5px;"><?php _e("Average (views/day)","rng-postviews"); ?></td>
-                <td style="padding: 6px 5px;"><?php echo $average_views_per_week; ?></td>
-            </tr>
-        </tbody>
-    </table>
+<div style="font-family: arial;">
+    <h3 style="display: block;border-bottom: 1px solid #bababa;text-align: center; width: 85%;margin: 15px auto;padding-bottom: 5px;">PostViews weekly Reporting</h3>
+    <div style="display: block;overflow: auto;">
+        <table style="width: 85%;text-align: center;border-collapse: collapse;margin: auto;">
+            <thead>
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <th><?php _e("Date", "rng-postviews"); ?></th>
+                    <th><?php _e("Post Views", "rng-postviews"); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo current($days_period) . __("(Today)", "rng-postviews") ?></td>
+                    <td style="padding: 6px 5px;"><?php echo current($days_postviews) . $views; ?></td>
+                </tr>
+                <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php echo next($days_period); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo next($days_postviews) . $views; ?></td>
+                </tr>
+                <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php _e("Current Week", "rng-postviews"); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo current($weeks_postviews) . $views; ?></td>
+                </tr>
+                <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
+                    <td style="padding: 6px 5px;"><?php _e("Average (views/day)", "rng-postviews"); ?></td>
+                    <td style="padding: 6px 5px;"><?php echo $average_views_per_week; ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
