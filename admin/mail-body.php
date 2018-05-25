5d1590c46306eb91904c5cd58a3b698ef6c2d04b
asabagh <a.sabagh72@gmail.com>
2018-05-26 00:07:43 +0430

    better style for mail body

--- admin/mail-body.php
+++ admin/mail-body.php
@@ -2,9 +2,9 @@
 $views = __(" views", "rng-postviews");
 ?>
 <div style="font-family: arial;">
-    <h3 style="display: block;border-bottom: 1px solid #bababa;text-align: center; width: 85%;margin: 15px auto;padding-bottom: 5px;">PostViews weekly Reporting</h3>
-    <div style="display: block;overflow: auto;">
-        <table style="width: 85%;text-align: center;border-collapse: collapse;margin: auto;">
+    <h3 style="display: block;border-bottom: 1px solid #bababa; width: 85%;margin: 15px 0;padding-bottom: 5px;"><?php _e("PostViews weekly Reporting","rng-postviews"); ?></h3>
+    <div style="display: block;overflow: 0;">
+        <table style="width: 85%;border-collapse: collapse;margin: 0;">
             <thead>
                 <tr style="border-bottom: 1px solid #ddd;">
                     <th><?php _e("Date", "rng-postviews"); ?></th>
@@ -13,7 +13,7 @@ $views = __(" views", "rng-postviews");
             </thead>
             <tbody>
                 <tr style="background: #f9f9f9;border-bottom: 1px solid #ddd;">
-                    <td style="padding: 6px 5px;"><?php echo current($days_period) . __("(Today)", "rng-postviews") ?></td>
+                    <td style="padding: 6px 5px;"><?php echo current($days_period) . __(" (Today)", "rng-postviews") ?></td>
                     <td style="padding: 6px 5px;"><?php echo current($days_postviews) . $views; ?></td>
                 </tr>
                 <tr  style="background: #fff;border-bottom: 1px solid #ddd;">
