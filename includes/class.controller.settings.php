4960ea4cc133028e9212ad95665b80c2ce99f1b3
asabagh <a.sabagh72@gmail.com>
2019-03-19 20:07:45 +0330

    refactor setting class

--- includes/class.controller.settings.php
+++ includes/class.controller.settings.php
@@ -1,17 +1,36 @@
 <?php
-if (!class_exists("ja_settings")) {
+if (!class_exists("rngja_settings")) {
 
-    class ja_settings {
+    class rngja_settings {
+
+        public $settings;
 
         public function __construct() {
-            if (is_admin()) {
-                add_action("admin_menu", array($this, "admin_menu"));
-                add_action("admin_init", array($this, "general_settings_init"));
-                //new 
-                add_action("admin_notices", array($this, "configure_notices"));
-                add_action("admin_init", array($this, "dismiss_configuration"));
-                add_filter('plugin_action_links_' . JA_PRU, array($this, 'add_setting_link'));
+            $this->settings = $this->get_ja_settings();
+            if (!is_admin()) {
+                return;
             }
+            add_action("admin_menu", array($this, "admin_menu"));
+            add_action("admin_init", array($this, "general_settings_init"));
+            add_action("admin_notices", array($this, "configure_notices"));
+            add_action("admin_init", array($this, "dismiss_configuration"));
+            add_filter('plugin_action_links_' . JA_PRU, array($this, 'add_setting_link'));
+        }
+
+        public function get_ja_settings() {
+            $ja_settings_array = array(
+                'legal_post_type' => array('post'),
+                'mail' => get_option('admin_email')
+            );
+
+            $ja_settings = get_option("ja_postviews_options");
+            if (empty($ja_settings)) {
+                return $ja_settings_array;
+            }
+
+            $ja_settings_array['legal_pt'] = (array) $ja_settings['legal_post_type'];
+            $ja_settings_array['mail'] = (string) $ja_settings['mail'];
+            return $ja_settings_array;
         }
 
         /**
@@ -43,11 +62,8 @@ if (!class_exists("ja_settings")) {
          * @param type $args
          */
         public function general_setting_email($args) {
-            $options = get_option("ja_postviews_options");
-            $mail = $options['mail'];
-            if (!empty($mail) || !isset($mail)) {
-                $mail = get_option('admin_email');
-            }
+            $settings = $this->settings;
+            $mail = sanitize_email($settings['mail']);
             ?>
             <input type="text" id="<?php echo $args['id']; ?>" name="<?php echo $args['name']; ?>" value="<?php echo $mail; ?>">
             <?php
@@ -57,27 +73,23 @@ if (!class_exists("ja_settings")) {
          * output of setting section ja-postviews-section-top
          */
         public function general_setting_section_top() {
-            _e("postviews setting page. please at the first select permission for post type", "rng-postviews");
+            _e("Post views setting page. please at the first select permission for post type", "rng-postviews");
         }
 
         /**
          * output of setting field ja-postviews-pt
-         * @param type $args
+         * @param Array $args
          */
         public function general_setting_legal_post_type($args) {
-            $active_post_type = get_option("ja_postviews_options");
-            if ($active_post_type == FALSE) {
-                $active_post_type = array("post");
-            } else {
-                $active_post_type = $active_post_type['legal_post_type'];
-            }
+            $settings = $this->settings;
+            $active_post_type = $settings['legal_post_type'];
+
             $pt_args = array('public' => TRUE);
             $post_types = get_post_types($pt_args, 'names');
             foreach ($post_types as $post_type):
+                $checked = '';
                 if (is_array($active_post_type)) {
                     $checked = (in_array($post_type, $active_post_type)) ? "checked" : "";
-                } else {
-                    $checked = '';
                 }
                 ?>
                 <label>
@@ -93,30 +105,45 @@ if (!class_exists("ja_settings")) {
          */
         public function configure_notices() {
             $dismiss = get_option("ja_configration_dissmiss");
-            if (!$dismiss) {
-                $notice = '<div class="updated"><p>' . __('RNG_postviews is activated, you may need to configure it to work properly.', 'rng-postviews') . ' <a href="' . admin_url('admin.php?page=ja_postviews-settings') . '">' . __('Go to Settings page', 'rng-postviews') . '</a> &ndash; <a href="' . add_query_arg(array('ja_dismiss_notice' => 'true', 'ja_nonce' => wp_create_nonce("ja_dismiss_nonce"))) . '">' . __('Dismiss', 'rng-postviews') . '</a></p></div>';
-                echo $notice;
+            if ($dismiss) {
+                return;
             }
+            $notice = '<div class="updated"><p>' . __('rng-postviewes is activated, you may need to configure it to work properly.', 'rng-postviews') . ' <a href="' . admin_url('admin.php?page=ja_postviews-settings') . '">' . __('Go to Settings page', 'rng-postviews') . '</a> &ndash; <a href="' . add_query_arg(array('ja_dismiss_notice' => 'true', 'ja_nonce' => wp_create_nonce("ja_dismiss_nonce"))) . '">' . __('Dismiss', 'rng-postviews') . '</a></p></div>';
+            echo $notice;
         }
 
         /**
-         * dismiss configuration notice is adding in ja_settings->configure_notices()
+         * check if configation dismiss or not
+         * @param String $verify_nonce
+         * @param String $dismiss_notice
+         * @param String $dismiss
+         * @param String $page
+         * @return boolean
+         */
+        public function check_dismiss_configuration($verify_nonce, $dismiss_notice, $dismiss, $page) {
+            if ((isset($verify_nonce, $dismiss, $dismiss_notice) and $dismiss == "true") or $page == "ja_postviews-settings") {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * dismiss configuration notice
          */
         public function dismiss_configuration() {
-            if (isset($_GET['ja_dismiss_notice']) and $_GET['ja_dismiss'] = 'true' and ( isset($_GET['ja_nonce']))) {
-                $verify_nonce = wp_verify_nonce($_GET['ja_nonce'], 'ja_dismiss_nonce');
-                if ($verify_nonce) {
-                    update_option("ja_configration_dissmiss", 1);
-                }
-            } elseif (isset($_GET['page']) and $_GET['page'] == "ja_postviews-settings") {
+            $verify_nonce = wp_verify_nonce($_GET['ja_nonce'], 'ja_dismiss_nonce');
+            $dismiss_notice = $_GET['ja_dismiss_notice'];
+            $dismiss = $_GET['ja_dismiss'];
+            $page = $_GET['page'];
+            if ($this->check_dismiss_configuration($verify_nonce, $dismiss_notice, $dismiss, $page)) {
                 update_option("ja_configration_dissmiss", 1);
             }
         }
 
         /**
-         * adding setting link to RNG_Postviews in plugin list
-         * @param type $links
-         * @return type array
+         * adding setting link to rng-postviewes in plugin list
+         * @param Array $links
+         * @return Array
          */
         public function add_setting_link($links) {
             $mylinks = array(
@@ -128,4 +155,5 @@ if (!class_exists("ja_settings")) {
     }
 
 }
-new ja_settings();
+global $rngja_postviewes;
+$rngja_postviewes = new rngja_settings();
